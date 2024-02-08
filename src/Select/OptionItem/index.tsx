400c27000f76673c4910d68fd9bb5343c8a5894f
Andres Babativa <carlos3k11@gmail.com>
2024-02-08 13:37:23 -0500

    feat: add OptionItem component and its stories

---
+++ src/Select/OptionItem/index.tsx
@@ -0,0 +1,21 @@
+import { Text } from "@inubekit/text";
+import { StyledOptionItem } from "./styles";
+
+export interface IOptionItemProps {
+  id: string;
+  label: string;
+}
+
+const OptionItem = (props: IOptionItemProps) => {
+  const { id, label } = props;
+
+  return (
+    <StyledOptionItem id={id}>
+      <Text textAlign="start" size="medium">
+        {label}
+      </Text>
+    </StyledOptionItem>
+  );
+};
+
+export { OptionItem };
---
+++ src/Select/OptionItem/styles.js
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+
+import { inube } from "@shared/tokens";
+
+export const StyledOptionItem = styled.li`
+  display: flex;
+  align-items: center;
+  align-self: stretch;
+  min-height: ${inube.spacing.s500};
+  border-left: ${inube.spacing.s050} solid transparent;
+  padding: ${() =>
+    `${inube.spacing.s050} ${inube.spacing.s200} ${inube.spacing.s050} ${inube.spacing.s150}`};
+  cursor: pointer;
+
+  border-left-width: ${inube.spacing.s050};
+  border-left-style: solid;
+  border-left-color: ${({ theme }) =>
+    theme?.color?.stroke?.light?.regular || inube.color.stroke.light.regular};
+
+  p {
+    color: ${({ theme }) =>
+      theme?.color?.text?.dark?.regular || inube.color.text.dark.regular};
+  }
+
+  &:hover {
+    border-left-color: ${({ theme }) =>
+      theme?.color?.stroke?.primary?.regular ||
+      inube.color.stroke.primary.regular};
+
+    background-color: ${({ theme }) =>
+      theme?.color?.surface?.gray?.hover || inube.color.surface.gray.hover};
+
+    p {
+      color: ${({ theme }) =>
+        theme?.color?.text?.primary?.regular ||
+        inube.color.text.primary.regular};
+    }
+  }
+`;
