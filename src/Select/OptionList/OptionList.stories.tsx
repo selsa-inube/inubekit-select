c1fe9cdf31dae609cf5b189a96c5803c1f5b1d37
Andres Babativa <carlos3k11@gmail.com>
2024-02-08 15:44:43 -0500

    fix(OptionList): add story

---
+++ src/Select/OptionList/OptionList.stories.tsx
@@ -0,0 +1,33 @@
+import { props, parameters } from "./props";
+import { OptionList, OptionListProps } from "./index";
+import { OptionItem } from "../OptionItem";
+import { action } from "@storybook/addon-actions";
+
+const story = {
+  title: "Inputs/Select/OptionList",
+  component: [OptionList],
+  parameters,
+  argTypes: props,
+};
+
+const options = [
+  { id: "1", label: "Item 1" },
+  { id: "2", label: "Item 2" },
+  { id: "3", label: "Item 3" },
+];
+
+const Default = (args: OptionListProps) => (
+  <OptionList {...args} onClick={action("onClick")}>
+    {options.map((optionItem) => (
+      <OptionItem
+        key={optionItem.id}
+        id={optionItem.id}
+        label={optionItem.label}
+      />
+    ))}
+  </OptionList>
+);
+
+export default story;
+
+export { Default };
