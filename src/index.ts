5426b36639f7d58cd65e5fc01368f4920021b871
Cesar Marin Alfonso <45011420+cmarin001@users.noreply.github.com>
2024-08-23 17:36:32 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export { Select } from "./Select";
 export { OptionList } from "./Select/OptionList";
 export { OptionItem } from "./Select/OptionItem";
 export type { ISelect } from "./Select";
+export type { IOption } from "./Select";
 export type { IOptionList } from "./Select/OptionList";
 export type { IOptionItem } from "./Select/OptionItem";
 export type { ISelectSize } from "./Select/props";
